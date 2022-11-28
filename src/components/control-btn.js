7021a1e38f9934f47b0e3e47cc62b02fbaeae0bb
sophkim <soph.b.kim@gmail.com>
2022-11-28 23:14:50 +0900

    checkStalled 확인

--- src/components/control-btn.js
+++ src/components/control-btn.js
@@ -1,15 +1,23 @@
-import {ReactComponent as PlayImg} from '../components/images/play.svg';
-import {ReactComponent as PauseImg} from '../components/images/pause.svg';
+import { ReactComponent as PlayImg } from "../components/images/play.svg";
+import { ReactComponent as PauseImg } from "../components/images/pause.svg";
 
-const ControlButton = ({type, onClick}) => {
-    switch (type) {
-        case 'pause':
-            return (<span className="c-button" onClick={onClick}><PlayImg/></span>)
-        case 'play':
-            return (<span className="c-button" onClick={onClick}><PauseImg/></span>)
-        default:
-            return null;
-    }
-}
+const ControlButton = ({ type, onClick }) => {
+  switch (type) {
+    case "pause":
+      return (
+        <span className="c-button" onClick={onClick}>
+          <PlayImg />
+        </span>
+      );
+    case "play":
+      return (
+        <span className="c-button" onClick={onClick}>
+          <PauseImg />
+        </span>
+      );
+    default:
+      return null;
+  }
+};
 
 export default ControlButton;
